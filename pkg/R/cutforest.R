6d2d037ec3347d6a727349bfb5ca0424dbeb5faa
msghsajjad <msghsajjad@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-10-12 22:32:29 +0000

    cutforest is added like cutree for hclust.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hbiclust@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/cutforest.R
+++ pkg/R/cutforest.R
@@ -1,10 +1,12 @@
-cutforest <- function(b, K)
+cutforest <- function(b, k = NULL, h = NULL)
 {
+  if (is.null(k) && is.null(h)) 
+    stop("either 'k' or 'h' must be specified")
+  
   rdend<-list( "merge"=b$merge[b$row_col==0,],
                "height"=b$height[b$row_col==0],
                "order"= b$row_order,
-               "labels"=NULL,
-               "method"="complete",    
+               "method"="ward",    
                "dist.method"="euclidean");
   class(rdend)<-"hclust";
   #cutree(rdend, k=3)
@@ -14,18 +16,46 @@ cutforest <- function(b, K)
   cdend<-list( "merge"=b$merge[b$row_col==1,],
                "height"=b$height[b$row_col==1],
                "order"= b$col_order,
-               "labels"=NULL,
-               "method"="complete",
+               "method"="ward",
                "dist.method"="euclidean");
   class(cdend)<-"hclust";
   # cdend<- as.dendrogram(cdend)
   # b$col_order<- order.dendrogram(cdend)
-  rind=cutree(rdend, k=K)
-  cind=cutree(cdend, k=K)
-  groups <- list("row-group"=cbind(b$row_name, rind), "col-group"=cbind(b$col_name, cind) )
+  
+  if ( is.null(h) )
+  {
+    i=1
+    trow=b$dim[1];
+    tcol=b$dim[2];
+    while((trow*tcol)>k)
+    {        
+      if((b$row_col[i]==0) && (trow>1))
+        trow=trow-1;
+      if((b$row_col[i]==1) && (tcol>1))
+        tcol=tcol-1;
+      i=i+1
+    }
+    
+    rind=cutree(rdend, k=trow)
+    cind=cutree(cdend, k=tcol)
+  }
+  else
+  {
+    rind=cutree(rdend, h=h)
+    cind=cutree(cdend, h=h)
+  }
+  
+  if(is.null(b$row_name))
+    row.group <- rind
+  else
+    row.group <- data.frame(b$row_name, rind)
+  
+  if(is.null(b$col_name))
+    col.group <- cind
+  else
+    col.group <- data.frame(b$col_name, cind)
+  
+  groups <- list(row.group, col.group)
   class(groups) <- "bigroup";
   return(groups)
 }
-
-
-
