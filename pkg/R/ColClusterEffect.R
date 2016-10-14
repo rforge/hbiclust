623c59a18aa727c6e8a5a5dfde974ad7bf4ef993
msghsajjad <msghsajjad@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-10-14 02:30:50 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hbiclust@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ColClusterEffect.R
+++ pkg/R/ColClusterEffect.R
@@ -3,17 +3,17 @@ ColClusterEffect <- function(b, rind, cind)
   lkc = rep(0, max(cind))
   for(ci in 1:max(cind))
   {
-    print(ci)
+    #print(ci)
     for(ri in 1:max(rind))
     {
-      print(ri)
+      #print(ri)
       tdat=b$data[rind==ri, cind==ci];
       if(class(tdat)=="matrix")                    
       {
         mns = rowMeans(tdat);
         lkc[ci] = lkc[ci]+(sum(sweep(tdat, 1, mns)^2)/nrow(tdat));
       }
-      print((sum(sweep(tdat, 1, mns)^2)/nrow(tdat)))
+      #print((sum(sweep(tdat, 1, mns)^2)/nrow(tdat)))
     }
     tdat=b$data[, cind==ci];
     if(class(tdat)=="matrix")                    
@@ -21,7 +21,7 @@ ColClusterEffect <- function(b, rind, cind)
       mns = colMeans(tdat);
       lkc[ci] = lkc[ci]-(sum(sweep(tdat, 2, mns)^2)/ncol(tdat));
     }
-    print((sum(sweep(tdat, 2, mns)^2)/ncol(tdat)))
+    #print((sum(sweep(tdat, 2, mns)^2)/ncol(tdat)))
   }
   return(-lkc)
 }
