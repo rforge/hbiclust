89e32176651e1a5159c1867834e3c8376e1809ad
msghsajjad <msghsajjad@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-10-21 04:14:15 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/hbiclust@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/clusterimp.R
+++ pkg/R/clusterimp.R
@@ -19,7 +19,12 @@ clusterimp <- function(b, rind, cind)
       lkc[ci] = lkc[ci]-(sum(sweep(tdat, 2, mns)^2)/nrow(tdat));
     }
   }
-  
+  clc=cind
+  for(ci in 1:max(cind))
+  {
+   clc[clc==ci]=-lkc[ci]
+  }
+  ###########################################################
   lkr = rep(0, max(rind))
   for(ri in 1:max(rind))
   {
@@ -39,21 +44,46 @@ clusterimp <- function(b, rind, cind)
       lkr[ri] = lkr[ri]-(sum(sweep(tdat, 1, mns)^2)/ncol(tdat));
     }
   }
-  mns = rowMeans(b$data);
-  rl=rowSums(sweep(b$data, 1, mns)^2)/ncol(b$data);
+  
   rlc=rind
   for(ri in 1:max(rind))
   {
-    rlc[rlc==ri]=-lkr[ri]
+   rlc[rlc==ri]=-lkr[ri]
   }
-  
-  mns = colMeans(b$data);
-  cl=colSums(sweep(b$data, 2, mns)^2)/nrow(b$data);
-  clc=cind
-  for(ci in 1:max(cind))
+  ###################################################################
+  rl = rep(0, length(rind))
+  for(ri in 1:length(rind))
   {
-    clc[clc==ci]=-lkc[ci]
+    for(ci in 1:max(cind))
+    {
+      tdat=b$data[ri, cind==ci];
+      rl[ri] = rl[ri]+var(tdat);
+    }
+    tdat=b$data[ri,];
+    rl[ri] = rl[ri]-var(tdat)
   }
+  rl = -rl
   
-  return(list("Max row"=rbind(max(rl), max(rlc)),"Row likelihood"=rbind(rl/max(rl), rlc/max(rlc)), "Max col"=rbind(max(cl), max(clc)), "Col likelihood"=rbind(cl/max(cl), clc/max(clc))))
+  cl = rep(0, length(cind))
+  for(ci in 1:length(cind))
+  {
+    for(ri in 1:max(rind))
+    {
+      tdat=b$data[rind==ri, ci];
+      cl[ci] = cl[ci]+var(tdat);
+    }
+    tdat=b$data[,ci];
+    cl[ci] = cl[ci]-var(tdat)
+  }
+  cl = -cl
+  # mns = colMeans(b$data);
+  # cl=colSums(sweep(b$data, 2, mns)^2)/nrow(b$data);
+  # clc=cind
+  # for(ci in 1:max(cind))
+  # {
+  #   clc[clc==ci]=-lkc[ci]
+  # }
+  # 
+  #return(list("max_row"=rbind(max(rl), max(rlc)),"row_likelihood"=rbind(rl/max(rl), rlc/max(rlc)), "max_col"=rbind(max(cl), max(clc)), "col_likelihood"=rbind(cl/max(cl), clc/max(clc))))
+  return(list("row_likelihood"=rbind(rl, rlc), "col_likelihood"=rbind(cl, clc)))
 }
